e8a647adfe7e64a46d13182d0cfa2a6c9676eeb8
hk <893272483@qq.com>
2020-12-16 09:41:45 +0800

    '12/16'

--- src/views/teacher/external_exchange/exchange_content/exchange.vue
+++ src/views/teacher/external_exchange/exchange_content/exchange.vue
@@ -76,8 +76,7 @@
                 :before-remove="beforeRemove"
                 multiple
                 :limit="5"
-                :on-exceed="handleExceed"
-                :file-list="fileList">
+                :on-exceed="handleExceed">
             <el-button size="small" type="primary">点击上传</el-button>
             <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
         </el-upload>
@@ -95,7 +94,6 @@
                     label: '2020-2021第一学期'
                 }],
                 tableData: [],
-                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
             };
         },
         methods: {
