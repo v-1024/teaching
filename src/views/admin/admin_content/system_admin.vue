2e136d107e8d2ae0c63c8682ca9cb4f5d80bfd77
hk <893272483@qq.com>
2020-12-18 13:15:52 +0800

    '12/18'

--- src/views/admin/admin_content/system_admin.vue
+++ src/views/admin/admin_content/system_admin.vue
@@ -12,30 +12,38 @@
                             clearable
                             v-model="formInline.col_name"
                             :fetch-suggestions="querySearch"
-                            placeholder="输入学院"
-                            @select="handleSelect"></el-autocomplete>
+                            placeholder="输入学院"></el-autocomplete>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" icon="el-icon-plus" @click="col_add">确认</el-button>
+                <el-button type="primary" icon="el-icon-plus" @click="col_add(1)">确认</el-button>
             </el-form-item>
         </el-form>
+
+        <el-dialog title="编辑学院/系部信息" :visible.sync="dialogFormVisible" :before-close="before_close">
+            <el-form>
+                <el-form-item label="学院" label-width="120px">
+                    <el-input v-model="formInline.col_name" style="width: 400px" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="系部" label-width="120px">
+                    <el-input v-model="formInline.new_dep" style="width: 400px" clearable></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="save">确 定</el-button>
+            </div>
+        </el-dialog>
 
         <el-table
                 :data="tableData"
                 stripe
-                style="width: 100%"
-                >
+                style="width: 100%">
 
             <!--系部信息、操作-->
             <el-table-column type="expand">
                 <template slot-scope="props">
                     <el-table stripe border :data="props.row.dep">
-                        <el-table-column label="系部" prop="dep_name" align="center">
-                            <template scope="scope">
-                                <el-input v-model="scope.row.dep_name" v-show="!scope.row.dep_edit"></el-input>
-                                <span v-show="scope.row.dep_edit">{{scope.row.dep_name}}</span>
-                            </template>
-                        </el-table-column>
+                        <el-table-column label="系部" prop="dep_name" align="center"></el-table-column>
                         <el-table-column label="状态" prop="dep_state" align="center">
                             <!--作用域插槽-->
                             <template slot-scope="scope">
@@ -47,14 +55,7 @@
                                 <el-button
                                         size="medium"
                                         type="primary"
-                                        v-show="scope.row.dep_btn"
                                         @click="dep_editor(scope.$index, scope.row)">编辑</el-button>
-
-                                <el-button
-                                        size="medium"
-                                        type="primary"
-                                        v-show="!scope.row.dep_btn"
-                                        @click="dep_saver(scope.$index, scope.row)">保存</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
@@ -65,8 +66,10 @@
             <!--学院信息、操作-->
             <el-table-column label="学院" prop="col_name" align="center">
                 <template scope="scope">
-                    <el-input v-model="scope.row.col_name" v-show="!scope.row.col_edit" style="width: 200px;"></el-input>
-                    <span v-show="scope.row.col_edit">{{scope.row.col_name}}</span>
+                    <div style="height: 42px ; line-height: 42px">
+                        <el-input v-model="scope.row.col_name" v-show="!scope.row.col_edit" class="input"></el-input>
+                        <span v-show="scope.row.col_edit">{{scope.row.col_name}}</span>
+                    </div>
                 </template>
             </el-table-column>
             <el-table-column label="状态" prop="col_state" align="center">
@@ -77,7 +80,8 @@
             </el-table-column>
             <el-table-column label="操作" align="center">
                 <template slot-scope="scope">
-                    <el-button
+                    <div class="div">
+                        <el-button
                             size="medium"
                             type="primary"
                             v-show="scope.row.col_btn"
@@ -88,6 +92,7 @@
                             type="primary"
                             v-show="!scope.row.col_btn"
                             @click="col_saver(scope.$index, scope.row)">保存</el-button>
+                    </div>
                 </template>
             </el-table-column>
         </el-table>
@@ -99,11 +104,14 @@
         name: "system_admin",
         data() {
             return {
+                flag: '0' ,
                 college: [],
                 formInline: {
                     term: '',
                     col_name: '',
+                    new_dep: ''
                 },
+                dialogFormVisible: false ,
                 tableData: [
                     {
                         col_name: '计算机科学与工程学院',
@@ -115,14 +123,10 @@
                                 {
                                     dep_name: '网络工程系',
                                     dep_state: true,
-                                    dep_edit: true,
-                                    dep_btn: true
                                 },
                                 {
                                     dep_name: '软件工程系',
                                     dep_state: false,
-                                    dep_edit: true,
-                                    dep_btn: true
                                 }
                             ]
                     },
@@ -136,8 +140,6 @@
                                 {
                                     dep_name: '体育教育系',
                                     dep_state: true,
-                                    dep_edit: true,
-                                    dep_btn: true
                                 }
                             ]
                     }
@@ -148,7 +150,12 @@
             this.college = this.loadAll();
         } ,
         methods: {
-
+            loadAll() {      //后端获取
+                return [
+                    { "value": "计算机科学与工程学院"},
+                    { "value": "体育学院"}
+                ];
+            },
             querySearch(queryString, cb) {
                 const col = this.college;
                 const results = queryString ? col.filter(this.createFilter(queryString)) : col;
@@ -160,42 +167,45 @@
                     return (college.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
                 };
             },
-            loadAll() {
-                return [
-                    { "value": "计算机科学与工程学院"},
-                    { "value": "体育学院"}
-                ];
-            },
-            handleSelect(item) {
-                console.log(item);
-            } ,
-
-
             col_editor(index , row) {
                 this.tableData[index].col_edit = false;
                 this.tableData[index].col_btn = false
             } ,
-            col_saver(index , row) {
+            col_saver(index , row) {     //修改后上传到数据库,未修改则不发请求
                 this.tableData[index].col_edit = true;
                 this.tableData[index].col_btn = true
             } ,
             dep_editor(index , row) {
-                this.dep[index].dep_edit = false;
-                this.dep[index].dep_btn = false
-            } ,
-            dep_saver(index , row ) {
-                this.dep[index].dep_edit = true;
-                this.dep[index].dep_btn = true
+                //获取父级（学院）的信息
+                this.formInline.new_dep = row.dep_name;
+                this.flag = '0';
+                this.dialogFormVisible = true;
             } ,
             term_btn() {
-                this.$message({
-                    message: '学期设置成功！',
-                    type: 'success'
-                });
+                if (this.formInline.term == '')
+                    this.$message.warning('学期不能为空！');
+                else
+                    this.$message({
+                        message: '学期设置成功！',
+                        type: 'success'
+                    });
             } ,
             col_add() {
-                console.log(this.formInline.col_name);
-            }
+                this.dialogFormVisible = true;
+                this.flag = '1'
+            } ,
+            save() {     //插入新数据或更新系部信息
+                if (this.flag === '1')
+                    console.log('新增学院和系部');
+                else
+                    console.log('修改系部');
+                this.before_close();
+            } ,
+            before_close() {
+                this.formInline.col_name = '';
+                this.formInline.new_dep =  '';
+                this.dialogFormVisible = false;
+            } ,
         }
     }
 </script>
@@ -218,5 +228,16 @@
         margin-bottom: 0;
         width: 50%;
     }
+    .input {
+        height: 40px;
+        width: 200px;
+    }
 
+    .div {
+        height: 42px;
+        line-height: 42px;
+        position: absolute ;
+        bottom: 12px;
+        right:210px;
+    }
 </style>
